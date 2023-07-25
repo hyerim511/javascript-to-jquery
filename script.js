d0f4bb75a326a9d1579c033c8cdd21ba29e9db19
pinhaosama <thegabcraft@gmail.com>
2023-07-25 16:15:34 -0700

    fix dice roller imgs

--- script.js
+++ script.js
@@ -64,14 +64,14 @@ const win = 'imgs/win.png';
 
 const map = [
     [black, black, black, black, black, black, black, black, black, black, black],
-    [black, grass, path, path, path, path, path, path, path, tp, black],
-    [black, grass, path, grass, grass, grass, grass, grass, path, grass, black],
-    [black, grass, path, path, grass, path, path, path, path, grass, black],
-    [black, grass, grass, path, grass, path, grass, grass, grass, grass, black],
-    [black, grass, grass, path, grass, path, grass, grass, grass, grass, black],
-    [black, grass, path, path, grass, grass, grass, grass, grass, win, black],
-    [black, grass, path, grass, grass, grass, grass, grass, grass, path, black],
-    [black, tp2, path, path, path, path, path, path, path, path, black],
+    [black, grass, path , path , path , path , path , path , path , tp   , black],
+    [black, grass, path , grass, grass, grass, grass, grass, path , grass, black],
+    [black, grass, path , path , grass, path , path , path , path , grass, black],
+    [black, grass, grass, path , grass, path , grass, grass, grass, grass, black],
+    [black, grass, grass, path , grass, path , grass, grass, grass, grass, black],
+    [black, grass, path , path , grass, grass, grass, grass, grass, win  , black],
+    [black, grass, path , grass, grass, grass, grass, grass, grass, path , black],
+    [black, tp2  , path , path , path , path , path , path , path , path , black],
     [black, grass, grass, grass, grass, grass, grass, grass, grass, grass, black],
     [black, black, black, black, black, black, black, black, black, black, black],
 ]
