b6234f36e5ffabbc06b4e105263deb2e4f219841
hyerim <kimhyerim511@gmail.com>
2023-08-02 16:28:46 -0700

    change to Jquery

--- js/music.js
+++ js/music.js
@@ -1,17 +1,18 @@
 // Variables for Music Player
-let musicTitle = document.getElementById('music-title');
-let musicArtist = document.getElementById('music-artist');
-let musicPlay = document.getElementById('music-play');
-let musicPause = document.getElementById('music-pause');
-let musicForward = document.getElementById('music-forward');
-let musicBackward = document.getElementById('music-backward');
-let musicShuffle = document.getElementById('music-shuffle');
+// let musicTitle = document.getElementById('music-title');
+// let musicArtist = document.getElementById('music-artist');
+// let musicPlay = document.getElementById('music-play');
+// let musicPause = document.getElementById('music-pause');
+// let musicForward = document.getElementById('music-forward');
+// let musicBackward = document.getElementById('music-backward');
+// let musicShuffle = document.getElementById('music-shuffle');
 
 // Audio element to play
-let playMusic = document.createElement('audio');
+const playMusic = document.createElement('audio');
+// $('audio').addClass('musicPlayer');
 
 // Track List
-let trackList = [
+const trackList = [
     {
         title: "Awesome Game Music",
         artist: "HHH",
@@ -42,17 +43,39 @@ let trackList = [
 // Play music
 let trackNum = 0;
 
+// function setPlay(num){
+//     playMusic.src = trackList[num].path;
+//     musicTitle.innerHTML = trackList[num].title.toUpperCase();
+//     musicArtist.innerHTML = trackList[num].artist.toLowerCase();
+// }
 function setPlay(num){
     playMusic.src = trackList[num].path;
-    musicTitle.innerHTML = trackList[num].title.toUpperCase();
-    musicArtist.innerHTML = trackList[num].artist.toLowerCase();
+    $('#music-title').empty().append(trackList[num].title.toUpperCase());
+    $('#music-artist').empty().append(trackList[num].artist.toLowerCase());
 }
 
-musicPlay.addEventListener('click', ()=>{
-    musicPause.style.display = "block";
-    musicPlay.style.display = "none";
+function btnStyle(pause, play){
+    $('#music-pause').css("display", pause);
+    $('#music-play').css("display", play);
+}
+
+// musicPlay.addEventListener('click', ()=>{
+//     musicPause.style.display = "block";
+//     musicPlay.style.display = "none";
+//     setPlay(trackNum);
+//     playMusic.play();
+// });
+$('#music-play').click(function(){
+    btnStyle("block", "none");
     setPlay(trackNum);
     playMusic.play();
+
+    const progress = setInterval(function(){
+        let musicTime = Math.round(playMusic.currentTime);
+        let musicLength = Math.round(playMusic.duration);
+        let barWidth = (musicTime * 100) / musicLength;
+        $('#progress-bar').css("width", barWidth + '%');
+    }, 10);
 });
 
 // Play next track when it's ended
@@ -61,34 +84,68 @@ playMusic.addEventListener('ended', ()=>{
     setPlay(trackNum);
     playMusic.play();
 });
+// playMusic.on('ended', function() {
+//     trackNum++;
+//     setPlay(trackNum);
+//     playMusic.play();
+//     console.log("test");
+// });
+// $('.musicPlayer').on('ended', function() {
+//     trackNum++;
+//     setPlay(trackNum);
+//     playMusic.play();
+//     console.log("test");
+// });
 
 // Pause music
-musicPause.addEventListener('click', ()=>{
-    musicPause.style.display = "none";
-    musicPlay.style.display = "block";
+// musicPause.addEventListener('click', ()=>{
+//     musicPause.style.display = "none";
+//     musicPlay.style.display = "block";
+//     playMusic.pause();
+// });
+$('#music-pause').click(function(){
+    btnStyle("none", "block");
     playMusic.pause();
 });
 
 // Play previous music
-musicBackward.addEventListener('click', ()=>{
+// musicBackward.addEventListener('click', ()=>{
+//     trackNum--;
+//     if(trackNum < 0) {
+//         trackNum = trackList.length;
+//     }
+//     musicPause.style.display = "block";
+//     musicPlay.style.display = "none";
+//     setPlay(trackNum);
+//     playMusic.play();
+// });
+$('#music-backward').click(function() {
     trackNum--;
     if(trackNum < 0) {
         trackNum = trackList.length;
     }
-    musicPause.style.display = "block";
-    musicPlay.style.display = "none";
+    btnStyle("block", "none");
     setPlay(trackNum);
     playMusic.play();
 });
 
 // Play next music
-musicForward.addEventListener('click', ()=>{
+// musicForward.addEventListener('click', ()=>{
+//     trackNum++;
+//     if(trackNum > trackList.length) {
+//         trackNum = 0;
+//     }
+//     musicPause.style.display = "block";
+//     musicPlay.style.display = "none";
+//     setPlay(trackNum);
+//     playMusic.play();
+// });
+$('#music-forward').click(function() {
     trackNum++;
     if(trackNum > trackList.length) {
         trackNum = 0;
     }
-    musicPause.style.display = "block";
-    musicPlay.style.display = "none";
+    btnStyle("block", "none");
     setPlay(trackNum);
     playMusic.play();
 });
@@ -100,7 +157,35 @@ function shuffle(array) {
 
 // Shuffle play list
 let shuffleBool = false;
-musicShuffle.addEventListener('click', ()=>{
+// musicShuffle.addEventListener('click', ()=>{
+//     let shuffArr = [];
+//     let tempNum = 0;
+//     for(var i=0; i<trackList.length; i++){
+//         shuffArr.push(i);
+//     }
+//     // Toggle the shuffle button
+//     shuffleBool = !shuffleBool;
+//     if(shuffleBool) {
+//         shuffle(shuffArr);
+//         musicShuffle.style.backgroundColor = "#77c700";
+//         musicShuffle.style.color = "black";
+//     } else {
+//         musicShuffle.style.backgroundColor = "#242424";
+//         musicShuffle.style.color = "#77c700";
+//     }
+//     // console.log(shuffArr);
+//     // tempNum = Math.floor(Math.random() * (shuffArr.length + 1));
+//     musicPause.style.display = "block";
+//     musicPlay.style.display = "none";
+//     setPlay(shuffArr[tempNum]);
+//     playMusic.play();
+//     playMusic.addEventListener('ended', ()=>{
+//         tempNum++;
+//         setPlay(shuffArr[tempNum]);
+//         playMusic.play();
+//     })
+// });
+$('#music-shuffle').click(function() {
     let shuffArr = [];
     let tempNum = 0;
     for(var i=0; i<trackList.length; i++){
@@ -110,18 +195,18 @@ musicShuffle.addEventListener('click', ()=>{
     shuffleBool = !shuffleBool;
     if(shuffleBool) {
         shuffle(shuffArr);
-        musicShuffle.style.backgroundColor = "#77c700";
-        musicShuffle.style.color = "black";
+        $('#music-shuffle').addClass('shuffle-on');
     } else {
-        musicShuffle.style.backgroundColor = "#242424";
-        musicShuffle.style.color = "#77c700";
+        $('#music-shuffle').removeClass('shuffle-on');
     }
-    // console.log(shuffArr);
-    // tempNum = Math.floor(Math.random() * (shuffArr.length + 1));
-    musicPause.style.display = "block";
-    musicPlay.style.display = "none";
+    btnStyle("block", "none");
     setPlay(shuffArr[tempNum]);
     playMusic.play();
+    // playMusic.on('ended', function() {
+    //     tempNum++;
+    //     setPlay(shuffArr[tempNum]);
+    //     playMusic.play();
+    // })
     playMusic.addEventListener('ended', ()=>{
         tempNum++;
         setPlay(shuffArr[tempNum]);
