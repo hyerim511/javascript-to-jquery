5cc98588000a2aa6b9090f368f4903c18b7e3b07
Chinatsu0820 <01417gohan@gmail.com>
2023-08-03 00:46:02 -0700

    edit README and timer

--- js/timer.js
+++ js/timer.js
@@ -1,18 +1,55 @@
 let seconds = 1000 * 60; //1000 = 1 second in JS
 let timer;
+let timerE = $('#timer')[0]; // HTML Element
+//var timerE1 = $('#timer');  // jQuery object 
+
+/* //These are just for confirmation
+console.log("timerE1: " + timerE1);
+console.log("timerE1 typeof: " + typeof(timerE1)); // see what data type timerE1 is
+console.log("timerE2: " + timerE2);
+console.log("timerE2 typeof: " + typeof(timerE2));
+*/
+
+
+/* //this is needed in JS ver.
+const playUp = document.getElementById("playUp"); */
+
+//------------for the start-------------
+timerE.innerHTML = "" + seconds / 1000;
+
+
+//when a user clicks the button, the timer will start to count.
+//jQuery ver.
+$("#playUp").click(function(){
+  if (seconds == 60000) {
+    timer = setInterval(countDown, 1000);
+  }
+})
+
+/* //JS ver.
+playUp.addEventListener("click", () => {
+  if (seconds == 60000) {
+    timer = setInterval(countDown, 1000);
+  }
+});*/
 
 function countDown() {
-   if(seconds == 60000)
-     timer = setInterval(countDown, 1000)
-   seconds -= 1000;
-   document.getElementById("timer").innerHTML = "" + seconds/1000;
-   if (seconds <= 0) {
-       clearInterval(timer);
-       alert("Time up!");
-   }
-} //If seconds are equal or greater than 0, countdown until 1 minute has passed
-//Else, clear the timer and alert user of how many words they type per minute
+  seconds -= 1000;
+  /* //JS ver.
+  document.getElementById("timer").innerHTML = "" + seconds / 1000; */
+  timerE.innerHTML = "" + seconds / 1000;
 
-document.getElementById("timer").innerHTML= "" + seconds/1000;
+  if (seconds > 0 && seconds <= 10000) {
+    $('#timer').removeClass("text-info").addClass('text-warning');
+}
 
-countDown();
+  if (seconds === 0) {
+    clearInterval(timer);
+  }
+ 
+  if (timerE.innerHTML == "0"){
+    setTimeout(() => {
+      alert("Time up!");
+    }, 1);
+  }
+}
