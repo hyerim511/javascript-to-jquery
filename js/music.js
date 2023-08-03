142865b10367c12694a831bade630453f696a9e3
hyerim <kimhyerim511@gmail.com>
2023-08-02 19:48:42 -0700

    edit progress bar

--- js/music.js
+++ js/music.js
@@ -1,4 +1,4 @@
-// Variables for Music Player
+// -------------------------------- Variables for Music Player
 // let musicTitle = document.getElementById('music-title');
 // let musicArtist = document.getElementById('music-artist');
 // let musicPlay = document.getElementById('music-play');
@@ -7,11 +7,11 @@
 // let musicBackward = document.getElementById('music-backward');
 // let musicShuffle = document.getElementById('music-shuffle');
 
-// Audio element to play
+// -------------------------------- Audio element to play
 let playMusic = document.createElement('audio');
 playMusic.volume = 0.2;
 
-// Track List
+// -------------------------------- Track List
 const trackList = [
     {
         title: "Awesome Game Music",
@@ -40,9 +40,24 @@ const trackList = [
     },
 ];
 
-// Play music
+// -------------------------------- Default track number
 let trackNum = 0;
 
+// -------------------------------- Progress bar
+function progress() {
+    $('#music-progress').click(function(e){
+        playMusic.currentTime = e.offsetX / $('#music-progress').width() * 100;
+        // console.log(playMusic.currentTime);
+    });
+    setInterval(function(){
+        let musicTime = Math.round(playMusic.currentTime);
+        let musicLength = Math.round(playMusic.duration);
+        let barWidth = (musicTime * 100) / musicLength;
+        $('#progress-bar').css("width", barWidth + '%');
+    }, 10);
+}
+
+// -------------------------------- Setting to play
 // function setPlay(num){
 //     playMusic.src = trackList[num].path;
 //     musicTitle.innerHTML = trackList[num].title.toUpperCase();
@@ -52,13 +67,47 @@ function setPlay(num){
     playMusic.src = trackList[num].path;
     $('#music-title').empty().append(trackList[num].title.toUpperCase());
     $('#music-artist').empty().append(trackList[num].artist.toLowerCase());
+    progress();
 }
 
+// -------------------------------- Change button style
 function btnStyle(pause, play){
     $('#music-pause').css("display", pause);
     $('#music-play').css("display", play);
 }
 
+// -------------------------------- Dropdown for the music list
+function musicList(){
+    for(let i=0; i<trackList.length; i++){
+        const musicList = $('<li></li>');
+        const musicTitle = $('<a href="#"></a>').text(trackList[i].title);
+        musicTitle.addClass("dropdown-item").css("font-size", "1.5rem");
+        musicList.append(musicTitle);
+        $('#music-item').append(musicList);
+
+        musicTitle.click(function(){
+            btnStyle("block", "none");
+            setPlay(i);
+            playMusic.play();
+        });
+    }
+}
+musicList();
+
+// -------------------------------- Play music after click okay button in popup
+// musicPopup.addEventListener('click', ()=>{
+//     musicPause.style.display = "block";
+//     musicPlay.style.display = "none";
+//     setPlay(trackNum);
+//     playMusic.play();
+// });
+$('#playUp').click(function(){
+    btnStyle("block", "none");
+    setPlay(trackNum);
+    playMusic.play();
+});
+
+// -------------------------------- Play music
 // musicPlay.addEventListener('click', ()=>{
 //     musicPause.style.display = "block";
 //     musicPlay.style.display = "none";
@@ -69,42 +118,17 @@ $('#music-play').click(function(){
     btnStyle("block", "none");
     setPlay(trackNum);
     playMusic.play();
-
-    const progress = setInterval(function(){
-        let musicTime = Math.round(playMusic.currentTime);
-        let musicLength = Math.round(playMusic.duration);
-        let barWidth = (musicTime * 100) / musicLength;
-        $('#progress-bar').css("width", barWidth + '%');
-    }, 10);
 });
 
-musicPopup.addEventListener('click', ()=>{
-    musicPause.style.display = "block";
-    musicPlay.style.display = "none";
-    setPlay(trackNum);
-    playMusic.play();
-});
 
-// Play next track when it's ended
+// -------------------------------- Play next track when it's ended
 playMusic.addEventListener('ended', ()=>{
     trackNum++;
     setPlay(trackNum);
     playMusic.play();
 });
-// playMusic.on('ended', function() {
-//     trackNum++;
-//     setPlay(trackNum);
-//     playMusic.play();
-//     console.log("test");
-// });
-// $('.musicPlayer').on('ended', function() {
-//     trackNum++;
-//     setPlay(trackNum);
-//     playMusic.play();
-//     console.log("test");
-// });
 
-// Pause music
+// -------------------------------- Pause music
 // musicPause.addEventListener('click', ()=>{
 //     musicPause.style.display = "none";
 //     musicPlay.style.display = "block";
@@ -115,7 +139,7 @@ $('#music-pause').click(function(){
     playMusic.pause();
 });
 
-// Play previous music
+// -------------------------------- Play previous track
 // musicBackward.addEventListener('click', ()=>{
 //     trackNum--;
 //     if(trackNum < 0) {
@@ -136,7 +160,7 @@ $('#music-backward').click(function() {
     playMusic.play();
 });
 
-// Play next music
+// -------------------------------- Play next track
 // musicForward.addEventListener('click', ()=>{
 //     trackNum++;
 //     if(trackNum > trackList.length) {
@@ -157,12 +181,12 @@ $('#music-forward').click(function() {
     playMusic.play();
 });
 
-// Function to make shuffled array
+// -------------------------------- Function to make shuffled array
 function shuffle(array) {
     array.sort(() => Math.random() - 0.5);
 }
 
-// Shuffle play list
+// -------------------------------- Shuffle play list
 let shuffleBool = false;
 // musicShuffle.addEventListener('click', ()=>{
 //     let shuffArr = [];
@@ -209,11 +233,7 @@ $('#music-shuffle').click(function() {
     btnStyle("block", "none");
     setPlay(shuffArr[tempNum]);
     playMusic.play();
-    // playMusic.on('ended', function() {
-    //     tempNum++;
-    //     setPlay(shuffArr[tempNum]);
-    //     playMusic.play();
-    // })
+
     playMusic.addEventListener('ended', ()=>{
         tempNum++;
         setPlay(shuffArr[tempNum]);
